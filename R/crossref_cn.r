0959564cb2640bd5dd67502675a7de9dee6581a2
schamberlain <myrmecocystus@gmail.com>
2013-10-12 14:19:08 -0700

    added fxn for content negotatiion API

---
+++ R/crossref_cn.r
@@ -0,0 +1,32 @@
+#' Search the CrossRef Metatdata API.
+#' 
+#' @importFrom httr GET add_headers stop_for_status content
+#' @importFrom plyr compact
+#' @param doi Search by a single DOI or many DOIs.
+#' @param header Header.
+#' @details See \url{http://search.labs.crossref.org/help/api} for more info on this 
+#'   	Crossref API service.
+#' @seealso \code{\link{crossref_r}}, \code{\link{crossref_citation}}, \code{\link{crossref_search_free}}
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @examples \dontrun{
+#' crossref_cn(doi = "10.1126/science.169.3946.635")
+#' crossref_cn(doi = "10.1126/science.169.3946.635")
+#' }
+#' @export
+crossref_cn <- function(doi = NULL, header="bibtex")
+{
+  header <- switch(header, 
+                   rdfxml = "application/rdf+xml",
+                   rdfturtle = "text/turtle",
+                   citeprocjson = "application/vnd.citationstyles.csl+json",
+                   formattedtextcitation = "text/x-bibliography",
+                   ris = "application/x-research-info-systems",
+                   bibtex =	"application/x-bibtex",
+                   crossrefuixrefxml =	"application/vnd.crossref.unixref+xml",
+                   datacitexml =	"application/vnd.datacite.datacite+xml")
+  url <- sprintf("http://dx.doi.org/%s", doi)
+  args <- compact(list())
+  out <- GET(url, query=args, add_headers(Accept = header))
+  stop_for_status(out)
+  content(out, "text")
+}
