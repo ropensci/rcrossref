84d805495bfa824dade041c2dc03920089a7a4ee
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-02 16:57:28 -0700

    moved tests to tests in root dir

--- tests/testthat/test_crossref.R
+++ tests/testthat/test_crossref.R
@@ -1,6 +1,6 @@
 context("Testing crossref")
 
 test_that("Random crossref search returns correct result type", {
-	 random_list <- crossref_r(type = 'journal_article')
+	 random_list <- cr_r(type = 'journal_article')
 	 expect_is(random_list, "list")
 })
