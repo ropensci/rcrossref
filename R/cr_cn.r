37fd69362160e89c023508f39c30aa6014e7ebc2
Noam Ross <noam.ross@gmail.com>
2014-07-01 17:22:55 -0700

    DOI from failed call in cr_cn is retained.

--- R/cr_cn.r
+++ R/cr_cn.r
@@ -70,10 +70,10 @@ cr_cn <- function(dois,
 
   if(length(dois) > 1)
     lapply(dois, function(z) {
-      out = try(cn(z))
+      out = try(cn(z), silent=TRUE)
       if("try-error" %in% class(out)) {
         warning(paste0("Failure in resolving '", z, "'. See error detail in results."))
-        out <- out[[1]]
+        out <- list(doi=z, error=out[[1]])
       }
       return(out) 
     })
