42994984e252d1d7097194d013d0a7e3118cffd1
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-02 11:21:12 -0700

    fixed crossref test, but still ignoring tests in check as hanging for some reason but tests work when run separately line by line, weird

--- tests/testthat/test_crossref.R
+++ tests/testthat/test_crossref.R
@@ -1,11 +1,13 @@
 context("Testing crossref")
 
+random_list <- cr_r(type = 'journal_article')
+
 test_that("Random crossref search returns correct result type", {
-	 random_list <- cr_r(type = 'journal_article')
-	 expect_is(random_list, "list")
+	 expect_is(random_list, "character")
 })
 
+entry <- cr_cn("10.6084/m9.figshare.97218", "bibentry")
 
 test_that("We can parse fighshare DOIs into bibentry type", {
-   entry <- cr_cn("10.6084/m9.figshare.97218", "bibentry")
+  expect_is(entry, "bibentry")
 })
