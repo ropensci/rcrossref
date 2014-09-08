a373f1fd01c78190ad23124e36d8e389909befff
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-08 08:36:43 -0700

    changed to new url for free search service, and using jsonlite instead of RJSONIO

--- R/cr_search_free.r
+++ R/cr_search_free.r
@@ -1,7 +1,7 @@
 #' Search the CrossRef Metatdata for DOIs using free form references.
 #'
 #' @import httr
-#' @importFrom RJSONIO toJSON
+#' @importFrom jsonlite toJSON
 #' @export
 #'
 #' @param query Reference query; a character vector of length 1 or greater,
@@ -13,7 +13,8 @@
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @examples \dontrun{
 #' # search with title, author, year, and journal
-#' cr_search_free(query = "Piwowar Sharing Detailed Research Data Is Associated with Increased Citation Rate PLOS one 2007")
+#' cr_search_free(query = "Piwowar Sharing Detailed Research Data Is Associated with 
+#' Increased Citation Rate PLOS one 2007")
 #'
 #' cr_search_free(query="Renear 2012") # too few words, need at least 3
 #'
@@ -24,7 +25,9 @@
 #' cr_search(doi = doi)
 #'
 #' # Queries can be quite complex too
-#' cr_search_free("M. Henrion, D. J. Mortlock, D. J. Hand, and A. Gandy, \"A Bayesian approach to star-galaxy classification,\" Monthly Notices of the Royal Astronomical Society, vol. 412, no. 4, pp. 2286-2302, Apr. 2011.")
+#' cr_search_free("M. Henrion, D. J. Mortlock, D. J. Hand, and A. Gandy, \"A Bayesian 
+#' approach to star-galaxy classification,\" Monthly Notices of the Royal Astronomical Society, 
+#' vol. 412, no. 4, pp. 2286-2302, Apr. 2011.")
 #'
 #' # Lots of queries
 #' queries <- c("Piwowar sharing data PLOS one",
@@ -34,11 +37,9 @@
 #' cr_search_free(queries)
 #' }
 
-cr_search_free <- function(query,
-		url = "http://search.labs.crossref.org/links")
+cr_search_free <- function(query, url = "http://search.crossref.org/links")
 {
-	query2 <- RJSONIO::toJSON(c(query))
-	tt <- POST(url, body=query2)
+	tt <- POST(url, config = c(content_type_json(), accept_json()), body=jsonlite::toJSON(query))
   stop_for_status(tt)
   res <- content(tt, as = "text")
 	out <- fromJSON(res, simplifyWithNames = FALSE)$results
