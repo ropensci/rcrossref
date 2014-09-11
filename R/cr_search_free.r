f32a9df71ce53569ec94abc56ac1f75d1b42d19f
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-11 09:58:49 -0400

    fixed bug in cr_search_free closes #24

--- R/cr_search_free.r
+++ R/cr_search_free.r
@@ -44,10 +44,10 @@
 	tt <- POST(url, config = c(content_type_json(), accept_json()), body=jsonlite::toJSON(query))
   stop_for_status(tt)
   res <- content(tt, as = "text")
-	out <- fromJSON(res)$results
-	if(length(out)==1){ data.frame(out, stringsAsFactors = FALSE) } else
-		{
-			temp <- llply(out, data.frame, stringsAsFactors = FALSE)
-			rbind.fill(temp)
-		}
+	fromJSON(res)$results
+# 	if(length(out)==1){ data.frame(out, stringsAsFactors = FALSE) } else
+# 		{
+# 			temp <- llply(out, data.frame, stringsAsFactors = FALSE)
+# 			rbind.fill(temp)
+# 		}
 }
