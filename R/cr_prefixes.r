2f310aa1063e1910c5a2864689b5d16c9fafe129
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-09 14:37:15 -0700

    added cr_prefxies fxn, #26

---
+++ R/cr_prefixes.r
@@ -0,0 +1,57 @@
+#' Search CrossRef prefixes
+#'
+#' BEWARE: The API will only work for CrossRef DOIs.
+#'
+#' @export
+#'
+#' @param prefixes (character) Publisher prefixes, one or more in a vector or list. Required.
+#' @template args
+#' @template moreargs
+#' @param facet (logical) Include facet results. Facet data not yet included in output.
+#'
+#' @examples \dontrun{
+#' cr_prefixes(prefixes="10.1016")
+#' cr_prefixes(prefixes="10.1016", works=TRUE)
+#' cr_prefixes(prefixes=c('10.1016','10.1371','10.1023','10.4176','10.1093'))
+#' cr_prefixes(prefixes=c('10.1016','10.1371'), works=TRUE)
+#' cr_works(prefixes="10.1016", filter=c(has_full_text=TRUE), limit=5)
+#' cr_works(prefixes="10.1016", query='ecology', limit=4)
+#' cr_works(prefixes="10.1016", query='ecology', limit=4)
+#' 
+#' # facets
+#' ## if works is FALSE, then facets ignored
+#' cr_prefixes(prefixes="10.1016", works=FALSE, facet=TRUE)
+#' cr_prefixes(prefixes="10.1016", works=TRUE, facet=TRUE)
+#' cr_prefixes(prefixes=c('10.1016','10.1371'), works=TRUE, facet=TRUE)
+#' }
+
+`cr_prefixes` <- function(prefixes, query = NULL, filter = NULL, offset = NULL,
+  limit = NULL, sample = NULL, sort = NULL, order = NULL, facet=FALSE, works = FALSE, 
+  .progress="none", ...)
+{
+  filter <- filter_handler(filter)
+  facet <- if(facet) "t" else NULL
+  args <- cr_compact(list(query = query, filter = filter, offset = offset, rows = limit,
+                          sample = sample, sort = sort, order = order, facet = facet))
+
+  if(length(prefixes) > 1){
+    res <- llply(prefixes, prefixes_GET, args=args, works=works, ..., .progress=.progress)
+    out <- lapply(res, "[[", "message")
+    out <- if(works) do.call(c, lapply(out, function(x) lapply(x$items, parse_works))) else lapply(out, data.frame, stringsAsFactors=FALSE)
+    df <- rbind_all(out)
+    meta <- if(works) data.frame(prefix=prefixes, do.call(rbind, lapply(res, parse_meta)), stringsAsFactors = FALSE) else NULL
+    facets <- lapply(res, function(x) parse_facets(x$message$facets))
+    names(facets) <- prefixes
+    list(meta=meta, data=df, facets=facets)
+  } else {
+    tmp <- prefixes_GET(prefixes, args, works=works, ...)
+    out <- if(works) rbind_all(lapply(tmp$message$items, parse_works)) else data.frame(tmp$message, stringsAsFactors=FALSE)
+    meta <- if(works) data.frame(prefix=prefixes, parse_meta(tmp), stringsAsFactors = FALSE) else NULL
+    list(meta=meta, data=df, facets=parse_facets(tmp$message$facets))
+  }
+}
+
+prefixes_GET <- function(x, args, works, ...){
+  path <- if(works) sprintf("prefixes/%s/works", x) else sprintf("prefixes/%s", x)
+  cr_GET(endpoint = path, args, todf = FALSE, ...)
+}
