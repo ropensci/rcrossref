bb75fdbf33da27ac8a9f5abcb35592a8aaa230c3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-22 10:08:45 -0700

    changd cr_full to cr_full_links to more accurately describe fxn, #10

--- R/cr_full.R
+++ R/cr_full.R
@@ -7,28 +7,48 @@
 #' @param ... Further curl args
 #' @examples \donttest{
 #' # pdf link
-#' cr_full(doi = "10.5555/515151", "pdf")
+#' cr_full_links(doi = "10.5555/515151", "pdf")
 #' 
 #' # xml and plain text links
 #' out <- cr_works(filter=c(has_full_text = TRUE))
 #' dois <- out$data$DOI
-#' cr_full(dois[1], "xml")
-#' cr_full(dois[1], "plain")
-#' cr_full(dois[1], "all")
+#' cr_full_links(dois[1], "xml")
+#' cr_full_links(dois[1], "plain")
+#' cr_full_links(dois[1], "all")
 #' 
 #' # No links
-#' cr_full(doi = cr_r(1), "xml")
+#' cr_full_links(doi = cr_r(1), "xml")
+#' 
+#' # Get full text
+#' #### DOESN"T WORK YET
+#' ## get DOIs first
+#' out <- cr_works(filter=c(has_full_text = TRUE, license_url='http://creativecommons.org/licenses/by/3.0/deed.en_US'))
+#' out <- cr_works(filter=c(license_url='http://creativecommons.org/licenses/by/3.0/deed.en_US'))
+#' out <- cr_works(filter=c(has_full_text = TRUE))
+#' 
+#' out <- cr_members(member_ids = 2258, works = TRUE)
+#' dois <- out$data$DOI
+#' xml <- cr_full_links(dois[1], "xml")
+#' cr_full_text(url = xml)
 #' }
 
-cr_full <- function(doi, type='xml', ...)
+cr_full_links <- function(doi, type='xml', ...)
 {
   url <- sprintf("http://dx.doi.org/%s", doi)
   response <- GET(url, hd(), ...)
   assert_that(response$headers$`content-type` == hd()$httpheader[[1]])
   tt <- response$headers$link
-  if(is.null(tt)) stop("No full text links", call. = FALSE)
-  get_type(tt, type)
+  if(is.null(tt)) NULL else get_type(x=tt, y=type)
 }
+
+# cr_full_text <- function(url, ...)
+# {
+#   res <- GET(url[[1]])
+#   assert_that(response$headers$`content-type` == hd()$httpheader[[1]])
+#   tt <- response$headers$link
+#   if(is.null(tt)) stop("No full text links", call. = FALSE)
+#   get_type(tt, type)
+# }
 
 hd <- function(header){
   add_headers(Accept = "application/vnd.crossref.unixsd+xml")
@@ -42,8 +62,9 @@ get_type <- function(x, y = 'xml') {
     lapply(withtype, "[[", "url")
   } else {
     y <- match.arg(y, c('xml','plain','pdf'))
-    y <- grep(y, c("text/xml","text/plain","application/pdf"), value = TRUE)
-    withtype[[y]]$url    
+    y <- grep(y, c("text/xml","text/plain","application/xml","application/pdf"), value = TRUE)
+    cr_compact(sapply(y, function(r) withtype[[r]]$url))
+#     withtype[[y]]$url
   }
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,21 @@
 cr_compact <- function(x) Filter(Negate(is.null), x)
 
 filter_handler <- function(x){
-  if(is.null(x)){ NULL } else{
+  if(is.null(x)){ NULL } else {
     nn <- names(x)
+#     nn <- match.arg(nn, filterchoices, TRUE)
+    if(any(nn %in% others)){
+      nn <- sapply(nn, function(x) {
+        if(x %in% others){
+          switch(x, 
+                 license_url='license.url',
+                 license_version='license.version',
+                 license_delay='license.delay',
+                 full_text_version='full-text.version',
+                 full_text_type='full-text.type')
+        } else { x }
+      }, USE.NAMES = FALSE)
+    }
     newnn <- gsub("_", "-", nn)
     names(x) <- newnn
     args <- list()
@@ -12,6 +25,17 @@ filter_handler <- function(x){
     paste0(args, collapse = ",")
   }
 }
+
+others <- c('license_url','license_version','license_delay','full_text_version','full_text_type')
+filterchoices <- c(
+  'has_funder','funder','prefix','member','from_index_date','until_index_date',
+  'from_deposit_date','until_deposit_date','from_update_date','until_update_date',
+  'from_first_deposit_date','until_first_deposit_date','from_pub_date','until_pub_date',
+  'has_license','license_url','license_version','license_delay','has_full_text',
+  'full_text_version','full_text_type','public_references','has_references','has_archive',
+  'archive','has_orcid','orcid','issn','type','directory','doi','updates','is_update',
+  'has_update_policy'
+)
 
 #' Crossref api internal handler
 #' 
