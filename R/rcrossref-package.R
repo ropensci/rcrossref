4a6b0fc5a9742def52e0275a9ebe22e136767c35
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-28 15:23:04 -0800

    added pkg level man file

---
+++ R/rcrossref-package.R
@@ -0,0 +1,14 @@
+#' R Client for Various CrossRef APIs.
+#' 
+#' rcrossref interacts with the main Crossref metadata search API at 
+#' \url{https://github.com/CrossRef/rest-api-doc/blob/master/rest_api.md}, the old metadata
+#' search API at \url{http://search.labs.crossref.org/}, the CrossRef OpenURL service 
+#' \url{http://www.crossref.org/openurl/}, and their DOI Content Negotiation  service at
+#' \url{http://www.crosscite.org/cn/}.
+#'
+#' @name rcrossref-package
+#' @aliases rcrossref
+#' @docType package
+#' @title R Client for Various CrossRef APIs.
+#' @keywords package
+NULL
