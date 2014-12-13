8a090b4fb0baaefa09c4fb226aa8e62d64898325
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-12 18:21:14 -0800

    suppressing warning in cr_citation fxn for tests

--- tests/testthat/test_cr_citation.R
+++ tests/testthat/test_cr_citation.R
@@ -1,14 +1,14 @@
 context("testing cr_citation")
 
 test_that("cr_citation returns correct class", {
-  expect_is(cr_citation(doi="10.1371/journal.pone.0042793"), "bibentry")
+  expect_is(suppressWarnings(cr_citation(doi="10.1371/journal.pone.0042793")), "bibentry")
 })
 
 test_that("cr_citation returns correct length", {
-  expect_equal(length(cr_citation(doi="10.1371/journal.pone.0042793")), 1)
+  expect_equal(length(suppressWarnings(cr_citation(doi="10.1371/journal.pone.0042793"))), 1)
 })
 
 test_that("cr_citation fails correctly", {
   library('httr')
-  expect_error(cr_citation(doi="10.1371/journal.pone.0042793", config=timeout(0.001)))
+  expect_error(suppressWarnings(cr_citation(doi="10.1371/journal.pone.0042793", config=timeout(0.001))))
 })
