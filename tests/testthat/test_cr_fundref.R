33838b7e2530e3b7c367ae803924eed5bccabe06
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-12 18:18:57 -0800

    cr_fundref tests, #33

---
+++ tests/testthat/test_cr_fundref.R
@@ -0,0 +1,26 @@
+context("testing cr_fundref")
+
+a <- out <- cr_fundref(dois=c('10.13039/100000001','10.13039/100000015'))
+b <- cr_fundref(dois='10.13039/100000001', works=TRUE, limit=5)
+
+test_that("cr_fundref returns correct class", {
+  expect_is(cr_fundref(query="NSF", limit=1), "list")
+  expect_is(a, "list")
+  expect_is(a[[1]]$data, "data.frame")
+  expect_is(a[[1]]$descendants, "character")
+  
+  expect_is(b, "list")
+  expect_is(b$data, "tbl_df")
+})
+
+test_that("cr_fundref dimensions are correct", {
+  expect_equal(length(a), 2)
+  expect_equal(NCOL(a[[1]]$data), 8)
+  expect_equal(length(b), 2)
+  expect_equal(NCOL(b$data), 21)
+})
+
+test_that("cr_fundref fails correctly", {
+  library('httr')
+  expect_warning(cr_fundref(dois='10.13039/100000001afasfasdf'))
+})
