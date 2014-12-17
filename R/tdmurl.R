958d830a846c63f6c37f6b5e6440c98a33f90f09
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-16 19:15:13 -0800

    fixed as.tdmurl params

--- R/tdmurl.R
+++ R/tdmurl.R
@@ -1,22 +1,25 @@
-makeurl <- function(x, y) structure(x, class = "tdmurl", type=y)
+#' Coerce a url to a tdmurl with a specific type
+#' 
+#' @export
+#' @param url A URL.
+#' @param type A document type, one of xml, pdf, or plain
+#' @examples 
+#' as.tdmurl("http://downloads.hindawi.com/journals/bmri/2014/201717.xml", "xml")
+#' as.tdmurl("http://downloads.hindawi.com/journals/bmri/2014/201717.pdf", "pdf")
+as.tdmurl <- function(url, type) UseMethod("as.tdmurl")
+
+#' @export
+#' @rdname as.tdmurl
+as.tdmurl.tdmurl <- function(url, type) url
+
+#' @export
+#' @rdname as.tdmurl
+as.tdmurl.character <- function(url, type) makeurl(check_url(url), type)
+
+#' @export
 print.tdmurl <- function(x, ...) {
   cat("<url> ", x[[1]], "\n", sep = "")
 }
 
-#' Coerce a url to a tdmurl with a specific type
-#' 
-#' @name as.tdmurl
-#' @examples 
-#' as.tdmurl("http://downloads.hindawi.com/journals/bmri/2014/201717.xml", "xml")
-#' as.tdmurl("http://downloads.hindawi.com/journals/bmri/2014/201717.pdf", "pdf")
-as.tdmurl <- function(x, y) UseMethod("as.tdmurl")
-
-#' @export
-#' @rdname as.tdmurl
-as.tdmurl.tdmurl <- function(x, y) x
-
-#' @export
-#' @rdname as.tdmurl
-as.tdmurl.character <- function(x, y) makeurl(check_url(x), y)
-
+makeurl <- function(x, y) structure(x, class = "tdmurl", type=y)
 check_url <- function(x) if(!grepl("http://", x)) stop("Not a proper url") else x
