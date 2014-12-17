462e1a1071ccae212adbf1fa95283b885835b863
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-16 16:23:47 -0800

    added man files and new R files for crossref TDM negotiation

---
+++ R/cr_full.R
@@ -0,0 +1,166 @@
+#' Get full text links from a DOI
+#' 
+#' @importFrom stringr str_extract
+#' @keywords internal
+#' @param doi A DOI
+#' @param type One of xml, plain, pdf, or all 
+#' @param ... Named parameters passed on to \code{\link[httr]{GET}}
+#' @examples \donttest{
+#' # pdf link
+#' cr_ft_links(doi = "10.5555/515151", "pdf")
+#' 
+#' # xml and plain text links
+#' out <- cr_works(filter=c(has_full_text = TRUE))
+#' dois <- out$data$DOI
+#' cr_ft_links(dois[2], "xml")
+#' cr_ft_links(dois[1], "plain")
+#' cr_ft_links(dois[1], "all")
+#' 
+#' # No links
+#' cr_ft_links(doi = cr_r(1), "xml")
+#' 
+#' # get full text
+#' ## elife
+#' out <- cr_members(4374, filter=c(has_full_text = TRUE), works = TRUE)
+#' ### pdf
+#' links <- cr_ft_links(out$data$DOI[1], "all")
+#' cr_ft_text(links, read=FALSE)
+#' cr_ft_text(links)
+#' ### xml
+#' links <- cr_ft_links(out$data$DOI[1], "all")
+#' cr_ft_text(links, 'xml')
+#' 
+#' ## pensoft
+#' out <- cr_members(2258, filter=c(has_full_text = TRUE), works = TRUE)
+#' ### pdf
+#' links <- cr_ft_links(out$data$DOI[1], "all")
+#' cr_ft_text(links, "pdf", read=FALSE)
+#' cr_ft_text(links, "pdf")
+#' ### xml
+#' cr_ft_text(links, 'xml')
+#' 
+#' ## hindawi
+#' out <- cr_members(98, filter=c(has_full_text = TRUE), works = TRUE)
+#' ### pdf
+#' links <- cr_ft_links(out$data$DOI[1], "all")
+#' cr_ft_text(links, "pdf", read=FALSE)
+#' cr_ft_text(links, "pdf")
+#' ### xml
+#' cr_ft_text(links, 'xml')
+#' 
+#' ## search for works with full text, and with CC-BY 3.0 license
+#' ### you can see available licenses with cr_licenses() function
+#' out <- 
+#'  cr_works(filter = list(has_full_text = TRUE,
+#'    license_url="http://creativecommons.org/licenses/by/3.0/"))
+#' links <- cr_ft_links(out$data$DOI[10], "all")
+#' cr_ft_text(links, 'xml')
+#' 
+#' ## elsevier - they don't actually give full text, ha ha, jokes on us!
+#' out <- cr_members(78, filter=c(has_full_text = TRUE), works = TRUE)
+#' links <- cr_ft_links(out$data$DOI[1], "all")
+#' cr_ft_text(links, 'xml') # notice how this is just metadata
+#' }
+
+cr_ft_links <- function(doi, type='xml', ...)
+{
+  url <- sprintf("http://dx.doi.org/%s", doi)
+  res <- GET(url, hd_turtle(), ...)
+  stopifnot(res$headers$`content-type` == hd_turtle()$httpheader[[1]])
+  tt <- res$headers$link
+  if(is.null(tt)) NULL else get_type(x=tt, y=type)
+}
+
+#' @export
+#' @rdname cr_ft_links
+cr_ft_text <- function(url, type='xml', path = "~/.crossref", overwrite = TRUE, read=TRUE, verbose=TRUE, ...)
+{
+  switch( pick_type(type, url),
+         xml = getTEXT(url$xml[[1]], type, ...),
+         plain = getTEXT(url$plain[[1]], type, ...),
+         pdf = getPDF(url$pdf[[1]], path, overwrite, type, read, verbose, ...)
+  )
+}
+
+pick_type <- function(x, z){
+  x <- match.arg(x, c("xml","plain","pdf"))
+  avail <- vapply(z, function(x) attr(x, which="type"), character(1), USE.NAMES = FALSE)
+  if(!x %in% avail) stop("Chosen type value not available in links", call. = FALSE)
+  x
+}
+
+getTEXT <- function(x, type, ...){
+  res <- GET(x, ...)
+  switch(type, 
+         xml = xmlParse(content(res, as = "text")),
+         plain = content(res, as = "text"))
+}
+
+getPDF <- function(url, path, overwrite, type, read, verbose, ...) {
+  if(!file.exists(path)) dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  filepath <- file.path(path, paste0(basename(url), ".", type))
+  if(verbose) message("Downloading pdf...")
+  res <- GET(url, accept("application/pdf"), write_disk(path = filepath, overwrite = overwrite), ...)
+  writepath <- res$request$writer[[1]]
+  if(read){
+    if(verbose) message("Exracting text from pdf...")
+    extract_xpdf(path=writepath, ...)
+  } else { 
+    writepath
+  }
+}
+
+hd <- function(header){
+  add_headers(Accept = "application/vnd.crossref.unixsd+xml")
+}
+
+hd_turtle <- function(header){
+  add_headers(Accept = "text/turtle")
+}
+
+get_type <- function(x, y = 'xml') {
+  res <- parse_urls(x)
+  withtype <- Filter(function(x) any("type" %in% names(x)), res)
+  withtype <- setNames(withtype, sapply(withtype, function(x) strsplit(x$type, "/")[[1]][[2]]))
+  if(y == "all"){
+    lapply(withtype, function(b) makeurl(b$url, st(b$type)))
+  } else {
+    y <- match.arg(y, c('xml','plain','pdf'))
+    makeurl(cr_compact(sapply(y, function(r) withtype[[r]]$url)), y)
+    # y <- grep(y, c("text/xml","text/plain","application/xml","application/pdf"), value = TRUE)
+  }
+}
+
+st <- function(x) strsplit(x, "/")[[1]][[2]]
+
+parse_urls <- function(x) {
+  lapply(strsplit(x, ",")[[1]], function(z) {
+    zz <- gsub("\\s", "", strsplit(z, ";")[[1]])
+    url <- str_extract(zz[1], "http://[\\?=:_&a-zA-Z\\./0-9-]+")
+    other <- sapply(zz[2:length(zz)], function(w) { 
+      nn <- gsub('\\"', "", strsplit(w, "=")[[1]])
+      setNames(nn[2], nn[1])
+    }, USE.NAMES=FALSE)
+    as.list(c(url=url, other))
+  })
+}
+
+makeurl <- function(x, y) structure(x, class = "tdmurl", type=y)
+print.tdmurl <- function(x, ...) {
+  cat("<url> ", x[[1]], "\n", sep = "")
+}
+
+as.url <- function(x, ...) UseMethod("as.url")
+#' @export
+as.url.character <- function(x, ...) paste0(do_base, "/", x)
+#' @export
+as.url.tdmurl <- function(x, ...) x
+
+# cr_full_text <- function(url, ...)
+# {
+#   res <- GET(url[[1]])
+# #   stopifnot(res$headers$`content-type` == hd()$httpheader[[1]])
+#   tt <- res$headers$link
+#   if(is.null(tt)) stop("No full text links", call. = FALSE)
+#   get_type(tt, type)
+# }
---
+++ R/extract_pdf.R
@@ -0,0 +1,64 @@
+#' Extract text from a single pdf document.
+#' 
+#' @import 
+#' @export
+#' @param path Path to a file
+#' @param ... further args passed on
+#' @return An object of class xpdf_char
+#' @details 
+#' Download xpdf from \url{http://www.foolabs.com/xpdf/download.html}
+#' @examples \donttest{
+#' path <- "~/github/sac/scott/pdfs/ChamberlainEtal2013Ecosphere.pdf"
+#' (res_xpdf <- extract_xpdf(path, "xpdf"))
+#' res_xpdf$meta
+#' res_xpdf$data
+#' }
+extract_xpdf <- function(path, ...){
+  cmds <- get_cmds(...)
+  path <- path.expand(path)
+  system2("pdftotext", shQuote(path))
+  newpath <- sub("\\.pdf", ".txt", path)
+  res <- paste(readLines(newpath, warn = FALSE), collapse = ", ")
+  meta <- pdf_info_via_xpdf(path)
+  structure(list(meta=meta, data=res), class="xpdf_char", path=path)
+}
+
+#' @export
+print.xpdf_char <- function(x, ...) {
+  cat("<document>", attr(x, "path"), "\n", sep = "")
+  cat("  Pages: ", x$meta$Pages, "\n", sep = "")
+  cat("  Title: ", x$meta$Title, "\n", sep = "")
+  cat("  Producer: ", x$meta$Producer, "\n", sep = "")
+  cat("  Creation date: ", as.character(as.Date(x$meta$CreationDate)), "\n", sep = "")
+}
+
+get_cmds <- function(...){
+  d <- list(...)
+  if(length(d)==0) "" else d
+}
+
+pdf_info_via_xpdf <- function(file, options = NULL){
+  outfile <- tempfile("pdfinfo")
+  on.exit(unlink(outfile))
+  status <- system2("pdfinfo", c(options, shQuote(normalizePath(file))), 
+                    stdout = outfile)
+  tags <- c("Title", "Subject", "Keywords", "Author", "Creator", 
+            "Producer", "CreationDate", "ModDate", "Tagged", "Form", 
+            "Pages", "Encrypted", "Page size", "File size", "Optimized", 
+            "PDF version")
+  re <- sprintf("^(%s)", 
+                paste(sprintf("%-16s", sprintf("%s:", tags)), collapse = "|"))
+  lines <- readLines(outfile, warn = FALSE)
+  ind <- grepl(re, lines)
+  tags <- sub(": *", "", substring(lines[ind], 1L, 16L))
+  info <- split(sub(re, "", lines), cumsum(ind))
+  names(info) <- tags
+  fmt <- "%a %b %d %X %Y"
+  if (!is.null(d <- info$CreationDate)) 
+    info$CreationDate <- strptime(d, fmt)
+  if (!is.null(d <- info$ModDate)) 
+    info$ModDate <- strptime(d, fmt)
+  if (!is.null(p <- info$Pages)) 
+    info$Pages <- as.integer(p)
+  info
+}
