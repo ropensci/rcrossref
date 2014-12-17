9158d0016a2e3bfceb708d95635830317332c9d5
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-17 10:12:18 -0800

    changed a file name, added note about full text thats not full text, closes #44

--- R/cr_ft_links.R
+++ R/cr_ft_links.R
@@ -4,7 +4,16 @@
 #' @param doi A DOI
 #' @param type One of xml, plain, pdf, or all 
 #' @param ... Named parameters passed on to \code{\link[httr]{GET}}
-#' @details Note that \code{\link{cr_ft_links}} is not vectorized.
+#' @details Note that \code{\link{cr_ft_links}} is not vectorized. 
+#' 
+#' Note that some links returned will not in fact lead you to full text 
+#' content as you would understandbly think and expect. That is, if you 
+#' use the \code{filter} parameter with e.g., \code{\link{cr_works}} and 
+#' filter to only full text content, some links may actually give back
+#' only metadata for an article. Elsevier is perhaps the worst offender, 
+#' for one because they have a lot of entries in Crossref TDM, but most
+#' of the links that are apparently full text are not in facct full text, 
+#' but only metadata. 
 #' @examples \dontrun{
 #' # pdf link
 #' cr_ft_links(doi = "10.5555/515151", "pdf")
