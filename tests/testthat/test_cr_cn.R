a4abbc7dab118fbe2dbf71ffffc99af36db02391
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-03 09:02:42 -0800

    removed some cr_cn tests giving 504 gateway timeout errors

--- tests/testthat/test_cr_cn.R
+++ tests/testthat/test_cr_cn.R
@@ -1,25 +1,25 @@
 context("testing cr_cn")
 
-a <- cr_cn(dois="10.1126/science.169.3946.635")
+# a <- cr_cn(dois="10.1126/science.169.3946.635")
 b <- cr_cn(dois="10.1126/science.169.3946.635", format="citeproc-json")
 c <- cr_cn("10.1126/science.169.3946.635", "rdf-xml")
 d <- cr_cn("10.1126/science.169.3946.635", "crossref-xml")
 e <- cr_cn("10.1126/science.169.3946.635", "bibentry")
 f <- cr_cn("10.1126/science.169.3946.635", "text", "apa")
-g <- cr_cn("10.1126/science.169.3946.635", "text", "oikos")
+# g <- cr_cn("10.1126/science.169.3946.635", "text", "oikos")
 
 test_that("cr_cn returns correct class", {
-  expect_is(a, "character")
+  # expect_is(a, "character")
   expect_is(b, "list")
   expect_is(c, "XMLInternalDocument")
   expect_is(d, "XMLInternalDocument")
   expect_is(e, "bibentry")
   expect_is(f, "character")
-  expect_is(g, "character")
+  # expect_is(g, "character")
 })
 
 test_that("cr_cn gives correct value", {
-  expect_match(a, "The Structure of Ordinary Water")
+  expect_match(b$subject[[1]], "General")
   expect_match(unclass(e)[[1]]$year, "1970")
 })
 
