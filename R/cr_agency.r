eed330a7585b407c721eb42746c7ba2dac8f213a
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-11 15:32:14 -0700

    messed a bit with egs in cr_agency, and fixed bug that was causing run_Examples to fail on cr_ft_text

--- R/cr_agency.r
+++ R/cr_agency.r
@@ -4,13 +4,11 @@
 #' 
 #' @param dois (character) One or more article or organization dois.
 #' @template moreargs
-#' @details BEWARE: The API will only work for CrossRef DOIs.
 #' @references \url{https://github.com/CrossRef/rest-api-doc/blob/master/rest_api.md}
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
 #' @examples \dontrun{
 #' cr_agency(dois = '10.13039/100000001')
-#' res <- cr_agency(dois = c('10.13039/100000001','10.13039/100000015'))
-#' res[['10.13039/100000015']]
+#' cr_agency(dois = c('10.13039/100000001','10.13039/100000015','10.5284/1011335'))
 #' }
 
 `cr_agency` <- function(dois = NULL, .progress="none", ...)
