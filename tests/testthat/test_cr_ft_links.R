e976254f9b6c599087cbbb178579caaaa73505ae
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-11 09:14:34 -0700

    fixed broken tests

--- tests/testthat/test_cr_ft_links.R
+++ tests/testthat/test_cr_ft_links.R
@@ -1,10 +1,9 @@
 context("testing cr_ft_links")
 
 a <- cr_ft_links(doi = "10.5555/515151", "pdf")
-out <- cr_works(filter=c(has_full_text = TRUE))
+out <- cr_works(filter = c(has_full_text = TRUE))
 dois <- out$data$DOI
 b <- cr_ft_links(dois[2], "pdf")
-c <- cr_ft_links(dois[1], "plain")
 d <- cr_ft_links(dois[1], "all")
 
 test_that("cr_ft_links returns correct class", {
@@ -22,7 +21,6 @@ test_that("cr_ft_links returns correct class", {
 test_that("cr_ft_links dimensions are correct", {
   expect_equal(length(a), 1)
   expect_equal(length(b), 1)
-  expect_equal(length(c), 1)
   expect_equal(length(d), 2)
   expect_equal(length(d[[1]]), 1)
 })
@@ -30,7 +28,7 @@ test_that("cr_ft_links dimensions are correct", {
 test_that("cr_ft_links gives back right values", {
   expect_equal(attr(a, "type"), "pdf")
   expect_equal(attr(b, "type"), "pdf")
-  expect_equal(attr(d[[2]], "type"), "plain")
+  expect_equal(attr(d[[2]], "type"), "xml")
 
   expect_null(cr_ft_links("10.1007/978-1-4302-1089-4"))
 })
