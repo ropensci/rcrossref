da9832d945d165874d41bb90802d53fe58f11351
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-03 06:09:56 -0700

    tidied code in cr_agency

--- R/cr_agency.r
+++ R/cr_agency.r
@@ -11,13 +11,12 @@
 #' cr_agency(dois = c('10.13039/100000001','10.13039/100000015','10.5284/1011335'))
 #' }
 
-`cr_agency` <- function(dois = NULL, .progress="none", ...)
-{
+`cr_agency` <- function(dois = NULL, .progress="none", ...) {
   foo <- function(x, y, ...){
-    cr_GET(endpoint = sprintf("works/%s/agency", x), args=list(), .progress=y, ...)
+    cr_GET(endpoint = sprintf("works/%s/agency", x), args = list(), .progress = y, ...)
   }
-  if(length(dois) > 1){
-    res <- llply(dois, foo, y=.progress, ...)
+  if (length(dois) > 1) {
+    res <- llply(dois, foo, y = .progress, ...)
     res <- lapply(res, "[[", "message")
     names(res) <- dois
     res
