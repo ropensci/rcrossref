f9459b5efc6ab9a3beccd4579c02a8d08d18fdfd
Scott Chamberlain <myrmecocystus@gmail.com>
2016-05-18 12:54:27 -0700

    covr to suggests, use new travis R setup

--- R/cr_ft_text.R
+++ R/cr_ft_text.R
@@ -260,7 +260,14 @@ getPDF <- function(url, path, auth, overwrite, type, read, verbose, cache=FALSE,
                write_disk(path = filepath, overwrite = overwrite),
                auth,
                config(followlocation = TRUE), ...)
-    filepath <- res$request$output$path
+    warn_for_status(res)
+    if (res$status_code < 202) {
+      filepath <- res$request$output$path
+    } else {
+      unlink(filepath)
+      filepath <- res$status_code
+      read <- FALSE
+    }
   }
 
   if (read) {
