5e857de0f1148f1ad4b76bd33ac80803c138a28b
Scott Chamberlain <myrmecocystus@gmail.com>
2016-05-21 10:41:25 -0700

    #108 fix documentation in cr_citation_count to remove PLOS references improve the docs as well bump dev version

--- R/cr_citation_count.r
+++ R/cr_citation_count.r
@@ -1,16 +1,18 @@
-#' Lookup article info via CrossRef with DOI and get a citation count.
+#' Get a citation count via CrossRef OpenURL
 #'
 #' @export
 #'
-#' @param doi digital object identifier for an article in PLoS Journals
-#' @param url the PLoS API url for the function (should be left to default)
-#' @param key your PLoS API key, either enter, or loads from .Rprofile
-#' @param ... Named parameters passed on to \code{\link[httr]{GET}}
+#' @param doi (character) digital object identifier for an article
+#' @param url (character) the url for the function (should be left to default)
+#' @param key your Crossref OpenURL email address, either enter, or loads 
+#' from \code{.Rprofile}. We use a default, so you don't need to pass this.
+#' @param ... Curl options passed on to \code{\link[httr]{GET}}
 #'
-#' @return citation count
+#' @return single numeric value - the citation count, or NA if not found or
+#' no count available
 #' @details See \url{http://labs.crossref.org/openurl/} for more info on this
 #' 		Crossref API service.
-#' @seealso \code{\link{cr_search}}, \code{\link{cr_r}}, \code{\link{cr_search_free}}
+#' @seealso \code{\link{cr_search}}, \code{\link{cr_r}}
 #' @author Carl Boettiger \email{cboettig@@gmail.com}
 #' @examples \dontrun{
 #' cr_citation_count(doi="10.1371/journal.pone.0042793")
@@ -20,15 +22,14 @@
 #' }
 
 cr_citation_count <- function(doi, url = "http://www.crossref.org/openurl/",
-	key = "cboettig@ropensci.org", ...)
-{
-  ## Assemble a url query such as:
+	key = "cboettig@ropensci.org", ...) {
+  
   args <- list(id = paste("doi:", doi, sep = ""), pid = as.character(key),
                noredirect = as.logical(TRUE))
   cite_count <- GET(url, query = args, make_rcrossref_ua(), ...)
   stop_for_status(cite_count)
   ans <- xml2::read_xml(ct_utf8(cite_count))
-  if (get_attr(ans, "status") == "unresolved") NA else as.numeric(get_attr(ans, "fl_count"))
+  if (get_attr(ans, "status") == "unresolved") as.numeric(NA_integer_) else as.numeric(get_attr(ans, "fl_count"))
 }
 
 get_attr <- function(xml, attr){
