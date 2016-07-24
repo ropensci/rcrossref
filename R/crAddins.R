8edf11cd6de1836441f912c4bf06c8d514918958
Hao Zhu <haozhu@hsl.harvard.edu>
2016-07-23 21:40:08 -0400

    Fix missing preview info for 2nd (or later) entry

--- R/crAddins.R
+++ R/crAddins.R
@@ -21,8 +21,17 @@ crAddins <- function() {
                    width = "100%", class="btn-primary")
     })
     
+    preview_message <- reactiveValues(added = 0, error = 0)
     output$preview <- function(){
-      req(input$entered_dois)
+      if(is.null(input$entered_dois))return(NULL)
+      if(input$entered_dois == "" & preview_message$added == 1){return(
+        HTML("<strong style='color:green;'>This citation has been saved ", 
+             "to crossref.bib</strong>")
+      )}
+      if(input$entered_dois == "" & preview_message$error == 1){return(
+        HTML("<strong style='color:red;'>Please make sure you have a valid ", 
+             "Crossref DOI before you save it.</strong>")
+      )}
       if(substr(input$entered_dois, 1, 3) != "10.")return(NULL)
       contents <- suppressWarnings(try(cr_cn(dois = input$entered_dois, 
                          format = "citeproc-json"), silent = TRUE))
@@ -55,16 +64,13 @@ crAddins <- function() {
       if(class(bib_to_write) != "try-error"){
         if(!"crossref.bib" %in% list.files()){file.create("crossref.bib")}
         write(paste0(bib_to_write, "\n"), "crossref.bib", append = T)
-        output$preview <- function(){
-          HTML("<strong style='color:green;'>This citation has been saved ", 
-               "to crossref.bib</strong>")
-        }
         updateTextInput(session, "entered_dois", value = "")
+        preview_message$added <- 1
+        preview_message$error <- 0
       }else{
-        output$preview <- function(){
-          HTML("<strong style='color:red;'>Please make sure you have a valid ", 
-               "Crossref DOI before you save it.</strong>")
-        }
+        updateTextInput(session, "entered_dois", value = "")
+        preview_message$added <- 0
+        preview_message$error <- 1
       }
     })
     
