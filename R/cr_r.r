a2fec15e56cd5245ab5c4208e2f489f88dcae9ba
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-19 21:52:25 -0700

    fix #118 sample param is now 100, not 1000

--- R/cr_r.r
+++ R/cr_r.r
@@ -2,21 +2,17 @@
 #'
 #' @export
 #' 
-#' @param sample The number of returned random DOIs. Maximum is 1000, default 20.
-#'    Note that a request for 1000 random DOIs will take a few seconds to
-#'    complete, whereas a request for 20 will take ~1 second.
+#' @param sample The number of returned random DOIs. Maximum: 100. Default: 20.
 #' @param ... Further args passed on to \code{\link{cr_works}}
-#' 
 #' @return A character vector of DOIs
-#' @references \url{http://random.labs.crossref.org/}
 #' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
-#' @seealso \code{\link{cr_search}}, \code{\link{cr_citation}}, 
-#' \code{\link{cr_search_free}}
 #' @examples \dontrun{
 #' # Default search gets 10 random DOIs
 #' cr_r()
+#' 
+#' # Get 30 DOIs
+#' cr_r(30)
 #' }
-
 `cr_r` <- function(sample = 10, ...) {
   tmp <- cr_works(sample = sample, ...)
   dois <- suppressWarnings(tryCatch(tmp$data$DOI, error = function(e) e))
--- man-roxygen/args.R
+++ man-roxygen/args.R
@@ -1,10 +1,11 @@
 #' @param query Query terms
 #' @param filter Filter options. See details.
 #' @param offset Number of record to start at, from 1 to infinity.
-#' @param limit Number of results to return in the query. Not relavant when searching with specific
-#' dois. Default: 20. Max: 1000
-#' @param sample (integer) Number of random results to return. when you use the sample parameter, 
-#' the rows and offset parameters are ignored. Ignored unless \code{works=TRUE}
-#' @param sort (character) Field to sort on, one of score, relevance, updated, deposited, indexed, 
-#' or published.
+#' @param limit Number of results to return in the query. Not relavant when 
+#' searching with specific dois. Default: 20. Max: 1000
+#' @param sample (integer) Number of random results to return. when you use 
+#' the sample parameter, the rows and offset parameters are ignored. 
+#' Ignored unless \code{works=TRUE}. Max: 100
+#' @param sort (character) Field to sort on, one of score, relevance, updated, 
+#' deposited, indexed, or published.
 #' @param order (character) Sort order, one of 'asc' or 'desc'
