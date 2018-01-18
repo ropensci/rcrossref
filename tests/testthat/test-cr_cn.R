465b2f65ad4f5afd2389714f7d487683c42e0311
Scott Chamberlain <myrmecocystus@gmail.com>
2018-01-18 12:47:41 -0800

    fix #147 - use bibtex::do_read_bib fxn to read bibtex files safely bump dev version number remove some cr_cn egs that dont work change to using https for CR api change to using htps://doi.org instead of doing the data.<agency>.org thing - seems to work fine

--- tests/testthat/test-cr_cn.R
+++ tests/testthat/test-cr_cn.R
@@ -8,21 +8,21 @@ test_that("cr_cn returns", {
   d <- cr_cn("10.1126/science.169.3946.635", "crossref-xml")
   e <- cr_cn("10.1126/science.169.3946.635", "bibentry")
   #f <- cr_cn(dois = "10.1126/science.169.3946.635", format = "text", style = "apa")
-  g <- cr_cn("10.5604/20831862.1134311", "crossref-tdm")
+  # g <- cr_cn("10.5604/20831862.1134311", "crossref-tdm")
   h <- cr_cn("10.3233/ISU-150780", "onix-xml")
 
   # correct classes
   expect_is(b, "list")
   expect_is(c, "xml_document")
   expect_is(d, "xml_document")
-  expect_is(e, "bibentry")
+  expect_is(e, "list")
   #expect_is(f, "character")
-  expect_is(g, "xml_document")
+  # expect_is(g, "xml_document")
   expect_is(h, "xml_document")
 
   # correct values
   expect_match(b$`container-title`, "Science")
-  expect_match(unclass(e)[[1]]$year, "1970")
+  expect_match(e$year, "1970")
 })
 
 test_that("cr_cn fails correctly", {
