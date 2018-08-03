40d227fab670ae1e80865bd3ab53cb84162195b0
Scott Chamberlain <myrmecocystus@gmail.com>
2018-08-03 12:51:09 -0700

    add assert() helper fxn

--- R/zzz.R
+++ R/zzz.R
@@ -109,6 +109,15 @@ check_number <- function(x) {
   }
 }
 
+assert <- function(x, y) {
+  if (!is.null(x)) {
+    if (!inherits(x, y)) {
+      stop(deparse(substitute(x)), " must be of class ",
+          paste0(y, collapse = ", "), call. = FALSE)
+    }
+  }
+}
+
 ifnullna <- function(x) {
   if (is.null(x)) NA else x
 }
