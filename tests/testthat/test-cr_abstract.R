d3e3f8b256c5f285f210c3ad8bbde3e51e00f9fb
Scott Chamberlain <myrmecocystus@gmail.com>
2020-10-02 08:55:29 -0700

    add tests for cr_abstract; tweak cr_Abstract egs

---
+++ tests/testthat/test-cr_abstract.R
@@ -0,0 +1,17 @@
+skip_on_cran()
+
+test_that("cr_abstract", {
+  vcr::use_cassette("cr_abstract", {
+    b <- cr_abstract("10.1175//2572.1")
+  })
+  
+  expect_is(b, "character")
+  expect_match(b, "University")
+  expect_gt(nchar(b), 1000L)
+})
+
+test_that("cr_abstract not found", {
+  vcr::use_cassette("cr_abstract_not_found", {
+    expect_error(cr_abstract('10.5284/1011335'))
+  })
+})
